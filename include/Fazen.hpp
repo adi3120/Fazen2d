075f96810740dc6e0716df809189b7a9d8fff1f6
Aditya yadav <83342917+adi3120@users.noreply.github.com>
2021-07-29 22:24:41 +0530

    Added Mouse_X() and Mouse_Y()

--- include/Fazen.hpp
+++ include/Fazen.hpp
@@ -57,6 +57,15 @@ protected:
     COORD buffersize = {short(s_width), short(s_height)};
     float center_x = 0;
     float center_y = 0;
+    float tempCenterx;
+    float tempCentery;
+    int consoleRangeStartx = 685;
+    int consoleRangeStarty = 30;
+    // int consoleRangeEndx = 1324;
+    // int consoleRangeEndy = 670;
+    POINT p;
+    int fontH;
+    int fontW;
 
 public:
     Fazen()
@@ -68,6 +77,17 @@ public:
 
         // Enable the window and mouse input events.
     }
+
+    void push()
+    {
+        tempCenterx = center_x;
+        tempCentery = center_y;
+    }
+    void pop()
+    {
+        center_x = tempCenterx;
+        center_y = tempCentery;
+    }
     float mapBounds(float value, float x1, float x2, float y1, float y2)
     {
         float m = (y2 - y1) / (x2 - x1);
@@ -75,6 +95,31 @@ public:
         value = (value - b) / m;
         return value;
     }
+    void GetWindowPos()
+    {
+        int x;
+        int y;
+        RECT rect = {};
+        GetWindowRect(GetConsoleWindow(), &rect);
+        x = rect.left;
+        y = rect.top;
+        consoleRangeStartx = x;
+        consoleRangeStarty = y;
+    }
+    float Mouse_X()
+    {
+        GetCursorPos(&p);
+        GetWindowPos();
+        p.x = mapBounds(p.x, 0, s_width, consoleRangeStartx, consoleRangeStartx + s_width * fontW);
+        return p.x;
+    }
+    float Mouse_Y()
+    {
+        GetCursorPos(&p);
+        p.y = mapBounds(p.y, 0, s_height, consoleRangeStarty, consoleRangeStarty + s_height * fontH);
+        return p.y - 3;
+    }
+
     const void display()
     {
         WriteConsoleOutputW(outhnd, buffscreen, buffersize, characterPos, &rect_win);
@@ -97,57 +142,82 @@ public:
         }
     }
     //BRESENHAM's LINE ALGO
-void drawLine(float x2, float y2, float x1, float y1, short col =whiteF)
-	{
-		float x, y, dx, dy, dx1, dy1, px, py, xe, ye, i;
-		dx = x2 - x1; dy = y2 - y1;
-		dx1 = std::abs(dx); dy1 = std::abs(dy);
-		px = 2 * dy1 - dx1;	py = 2 * dx1 - dy1;
-		if (dy1 <= dx1)
-		{
-			if (dx >= 0)
-				{ x = x1; y = y1; xe = x2; }
-			else
-				{ x = x2; y = y2; xe = x1;}
+    void drawLine(float x2, float y2, float x1, float y1, short col = whiteF)
+    {
+        float x, y, dx, dy, dx1, dy1, px, py, xe, ye, i;
+        dx = x2 - x1;
+        dy = y2 - y1;
+        dx1 = std::abs(dx);
+        dy1 = std::abs(dy);
+        px = 2 * dy1 - dx1;
+        py = 2 * dx1 - dy1;
+        if (dy1 <= dx1)
+        {
+            if (dx >= 0)
+            {
+                x = x1;
+                y = y1;
+                xe = x2;
+            }
+            else
+            {
+                x = x2;
+                y = y2;
+                xe = x1;
+            }
 
-			Plot(x, y,col);
-			
-			for (i = 0; x<xe; i++)
-			{
-				x = x + 1;
-				if (px<0)
-					px = px + 2 * dy1;
-				else
-				{
-					if ((dx<0 && dy<0) || (dx>0 && dy>0)) y = y + 1; else y = y - 1;
-					px = px + 2 * (dy1 - dx1);
-				}
-				Plot(x, y,col);
-			}
-		}
-		else
-		{
-			if (dy >= 0)
-				{ x = x1; y = y1; ye = y2; }
-			else
-				{ x = x2; y = y2; ye = y1; }
+            Plot(x, y, col);
 
-			Plot(x, y, col);
+            for (i = 0; x < xe; i++)
+            {
+                x = x + 1;
+                if (px < 0)
+                    px = px + 2 * dy1;
+                else
+                {
+                    if ((dx < 0 && dy < 0) || (dx > 0 && dy > 0))
+                        y = y + 1;
+                    else
+                        y = y - 1;
+                    px = px + 2 * (dy1 - dx1);
+                }
+                Plot(x, y, col);
+            }
+        }
+        else
+        {
+            if (dy >= 0)
+            {
+                x = x1;
+                y = y1;
+                ye = y2;
+            }
+            else
+            {
+                x = x2;
+                y = y2;
+                ye = y1;
+            }
 
-			for (i = 0; y<ye; i++)
-			{
-				y = y + 1;
-				if (py <= 0)
-					py = py + 2 * dx1;
-				else
-				{
-					if ((dx<0 && dy<0) || (dx>0 && dy>0)) x = x + 1; else x = x - 1;
-					py = py + 2 * (dx1 - dy1);
-				}
-				Plot(x, y, col);
-			}
-		}
-	}
+            Plot(x, y, col);
+
+            for (i = 0; y < ye; i++)
+            {
+                y = y + 1;
+                if (py <= 0)
+                    py = py + 2 * dx1;
+                else
+                {
+                    if ((dx < 0 && dy < 0) || (dx > 0 && dy > 0))
+                        x = x + 1;
+                    else
+                        x = x - 1;
+                    py = py + 2 * (dx1 - dy1);
+                }
+                Plot(x, y, col);
+            }
+        }
+    }
 
     void translate(float x, float y)
     {
@@ -200,7 +270,9 @@ public:
         SetConsoleWindowInfo(outhnd, TRUE, &rect_win);
         SetConsoleScreenBufferSize(outhnd, buffersize);
 
-        //SetConsoleMode(inhnd, ENABLE_EXTENDED_FLAGS | ENABLE_WINDOW_INPUT | ENABLE_MOUSE_INPUT);
+        SetConsoleMode(inhnd, ENABLE_EXTENDED_FLAGS | ENABLE_WINDOW_INPUT | ENABLE_MOUSE_INPUT);
+        fontH=fonth;
+        fontW=fontw;
     }
 
     const void drawRectangle(float x, float y, int b_width, int b_height, float angle = 0, short col = whiteF)
