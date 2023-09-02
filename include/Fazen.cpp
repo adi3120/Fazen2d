07014c03bbeb55e2bdb33d0f642e644a6552d0e5
Aditya yadav <83342917+adi3120@users.noreply.github.com>
2023-09-02 07:24:08 +0530

    Delete include directory

--- include/Fazen.cpp
+++
@@ -1,5 +0,0 @@
-#include "./Fazen.h"
-
-Fazen::Fazen(CHAR_INFO* buffscreen, int fontH, int fontW){
-	
-}
--- include/MouseHandler.h
+++
@@ -1,8 +0,0 @@
-#include<ConsoleHandler.h>
-class MouseHandler {
-public:
-	ConsoleHandler& console;
-    MouseHandler(ConsoleHandler& console); 
-    float GetMouseX();
-    float GetMouseY();
-};
